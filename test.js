385638f1b67f028f7e36cd3b53b3a4cf31a0c4c7
Adrian <105680173+adleal228@users.noreply.github.com>
2022-10-17 17:06:04 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,39 @@
 <script>
+  // contentScript.js
+function fetchResource(input, init) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage({input, init}, messageResponse => {
+      const [response, error] = messageResponse;
+      if (response === null) {
+        reject(error);
+      } else {
+        // Use undefined on a 204 - No Content
+        const body = response.body ? new Blob([response.body]) : undefined;
+        resolve(new Response(body, {
+          status: response.status,
+          statusText: response.statusText,
+        }));
+      }
+    });
+  });
+}
+
+// background.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  fetch(request.input, request.init).then(function(response) {
+    return response.text().then(function(text) {
+      sendResponse([{
+        body: text,
+        status: response.status,
+        statusText: response.statusText,
+      }, null]);
+    });
+  }, function(error) {
+    sendResponse([null, error]);
+  });
+  return true;
+});
+
 const collection = document.getElementsByClassName("TopBar__AlertsLink-tv8f1r-1");
 collection.style.display = "none";
 </script>
